111d86f64a98f735532db0994a222693d397fde8
David Caspar <david.caspar@aol.ch>
2019-07-30 23:17:05 +0200

    Enabled first unit test testing proper Spring initialization.

--- main/src/test/kotlin/ch/d4span/taskbutler/TaskButlerTest.kt
+++ main/src/test/kotlin/ch/d4span/taskbutler/TaskButlerTest.kt
@@ -1,10 +1,14 @@
 import ch.d4span.taskbutler.TaskButler
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.extension.ExtendWith
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.context.junit.jupiter.SpringExtension
 
+@ExtendWith(SpringExtension::class)
 @SpringBootTest(classes = [TaskButler::class])
-class BlogApplicationTests {
+class TaskButlerTest {
 
+    /** Tests if the Spring application context loads. **/
     @Test
     fun contextLoads() {
     }
