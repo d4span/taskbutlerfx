df004f40cf45f256fd85780d1902ae76e464dfda
David Caspar <david.caspar@aol.ch>
2019-08-01 16:02:15 +0200

    Added basic OpenJFX application.

--- main/src/main/kotlin/ch/d4span/taskbutler/TaskButler.kt
+++ main/src/main/kotlin/ch/d4span/taskbutler/TaskButler.kt
@@ -1,11 +1,28 @@
 package ch.d4span.taskbutler
 
+import javafx.application.Application
+import javafx.application.Platform
+import javafx.stage.Stage
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import javafx.scene.layout.StackPane
+import javafx.scene.Scene
+import javafx.scene.control.Label
+
 
 @SpringBootApplication
-open class TaskButler
+open class TaskButler : Application() {
+    override fun start(stage: Stage?) {
+        val javaVersion = System.getProperty("java.version")
+        val javafxVersion = System.getProperty("javafx.version")
+        val l = Label("Hello, JavaFX $javafxVersion, running on Java $javaVersion.")
+        val scene = Scene(StackPane(l), 640.0, 480.0)
+        stage?.setScene(scene)
+        stage?.show()
+    }
+}
 
 fun main(args: Array<String>) {
     SpringApplication.run(TaskButler::class.java, *args)
+    Application.launch(TaskButler::class.java)
 }
